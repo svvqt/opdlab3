4bf1d4a95799b08be5da81ac872a87eab259e58a
svvqt <125040694+svvqt@users.noreply.github.com>
2023-05-24 09:29:04 +0600

    Update web.py

--- web.py
+++ web.py
@@ -10,6 +10,7 @@ def index():
 
 @app.route('/login',methods=['GET','POST'])
 def login():
+    n = 0
     users, passwords = file.importingfile()
     global userlogin,userpassword
     if request.method=='POST':
@@ -19,9 +20,17 @@ def login():
         if userlogin not in users:
             return render_template('log_in_page.html', error='Пользователь не обнаружен')
         else:
-            if userpassword not in passwords:
-                return render_template('log_in_page.html',error='Неправильный пароль')
-    return render_template('user.html',login=userlogin,password=userpassword)
+            for i in users:
+                if userlogin==i:
+                    needlogin=i
+                    break
+                else:
+                    n+=1
+            needpass=passwords[n]
+            if userlogin==needlogin and userpassword==needpass:
+                return render_template('user.html', login=userlogin, password=userpassword)
+    return render_template('log_in_page.html', error='Неправильный пароль')
+
 @app.route('/regi',methods=['GET','POST'])
 def gotoreg():
     return render_template("reg.html")
@@ -31,13 +40,19 @@ def reg():
     if request.method=='POST':
         userlogin=str(request.form.get('login'))
         userpassword=str(request.form.get('password'))
-    f=open('users.txt','a')
-    f.write(userlogin+" ")
-    f.write(userpassword+'\n')
-    f.close()
-    return render_template('user.html',login=userlogin,password=userpassword)
+    users, passwords = file.importingfile()
+    if userlogin not in users:
+        f=open('users.txt','a')
+        f.write(userlogin+" ")
+        f.write(userpassword+'\n')
+        f.close()
+        return render_template('user.html',login=userlogin,password=userpassword)
+    else:
+        return render_template('reg.html',error='Имя пользователя занято')
 
-
+@app.route('/back',methods=['GET','POST'])
+def back():
+    return render_template('log_in_page.html')
 
 @app.route('/log_out')
 def log_out():
